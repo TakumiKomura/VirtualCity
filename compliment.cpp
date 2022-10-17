8187c82946ba01143d22ea46ec7655c4334831cf
TakumiKomura <takumi141219@outlook.jp>
2022-10-17 20:54:04 +0900

    Change variable name
    flag 名の変更
    namespaceの 定義

--- compliment.cpp
+++ compliment.cpp
@@ -2,25 +2,28 @@
 #include<iostream>
 #include<fstream>
 #include<vector>
+
 #define X 100
 #define Y 100
+
+using namespace std;
 
 struct Point{
     double x;
     double y;
     double z;
-    bool flag = 0; //建物ラベルとして初期化
+    bool isBuilding = true; //建物ラベルとして初期化
 };
 
 int main()
 {
-    std::string filename = "53394640_dsm_1m.dat";
-    std::ifstream file_in{filename}; //-std=c++17でコンパイル可能（file_in)
+    string filename = "53394640_dsm_1m.dat";
+    ifstream file_in{filename}; //-std=c++17でコンパイル可能（file_in)
     if(!file_in){
-        std::cerr << "ファイルを開けません："<< filename << "\n";
-        std::exit(1);
+        cerr << "ファイルを開けません："<< filename << "\n";
+        exit(1);
     }
-    std::vector<std::vector<Point>> geometry(X, std::vector<Point>(Y));
-    std::string line;
+    vector<vector<Point>> geometry(X, vector<Point>(Y));
+    string line;
 
 }
