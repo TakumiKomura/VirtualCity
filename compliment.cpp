7e41adbf55525c9a805cd0f333cbf90205ea4509
TakumiKomura <takumi141219@outlook.jp>
2022-10-19 07:46:52 +0900

    add time module
    タイム測定用のtime.hをincludeしました。測定開始と終了の宣言とタイムの出力部分を追加しました。コメントアウトしてあるので必要な時に使ってください。

--- compliment.cpp
+++ compliment.cpp
@@ -60,4 +60,8 @@ int main()
 
     input(geometry, file_in);
     output(geometry, file_out);
+
+    // clock_t start = clock();
+    // clock_t end = clock();
+    // cout << (double)(end - start) / CLOCKS_PER_SEC << " sec" << endl;
 }
