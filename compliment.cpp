1b03db6364316b19faeea0c7ae70ab16231dfa88
TakumiKomura <takumi141219@outlook.jp>
2022-10-18 22:19:19 +0900

    add output process
    出力処理を追加しました。
    input, output をそれぞれ関数化しました。

--- compliment.cpp
+++ compliment.cpp
@@ -15,17 +15,9 @@ struct Point{
     bool isBuilding = true; //建物ラベルとして初期化
 };
 
-int main()
+// read file and strage points as an array
+void input(vector<vector<Point>>& geometry, ifstream& file_in)
 {
-    string filename = "250_records.txt";
-    ifstream file_in{filename}; //-std=c++17でコンパイル可能（file_in)
-    if(!file_in){
-        cerr << "ファイルを開けません："<< filename << "\n";
-        exit(1);
-    }
-    vector<vector<Point>> geometry(ROW, vector<Point>(COL));
-
-    // 点の格納
     for (int i = 0; i < ROW; ++i)
     {
         for (int j = 0; j < COL; ++j)
@@ -33,12 +25,38 @@ int main()
             file_in >> geometry[i][j].x >> geometry[i][j].y >> geometry[i][j].z;
         }
     }
+}
 
+// write points to file
+void output(vector<vector<Point>>& geometry, ofstream& file_out)
+{
     for (int i = 0; i < ROW; ++i)
     {
         for (int j = 0; j < COL; ++j)
         {
-            cout << geometry[i][j].x << ' ' << geometry[i][j].y << ' ' << geometry[i][j].z << endl;
+            file_out << geometry[i][j].x << ' ' << geometry[i][j].y << ' ' << geometry[i][j].z << endl;
         }
     }
+}
+
+int main()
+{
+    string input_path = "250_records.txt";
+    ifstream file_in{input_path}; //-std=c++17でコンパイル可能（file_in)
+    if(!file_in){
+        cerr << "ファイルを開けません："<< input_path << "\n";
+        exit(1);
+    }
+
+    string output_path = "250_records_out.txt";
+    ofstream file_out{output_path}; //-std=c++17でコンパイル可能（file_in)
+    if(!file_out){
+        cerr << "ファイルを開けません："<< output_path << "\n";
+        exit(1);
+    }
+
+    vector<vector<Point>> geometry(ROW, vector<Point>(COL));
+
+    input(geometry, file_in);
+    output(geometry, file_out);
 }
