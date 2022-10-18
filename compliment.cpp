b025f7b61a3bef06ca3cd0fa5fd0462fa9d84468
TakumiKomura <takumi141219@outlook.jp>
2022-10-18 22:05:04 +0900

    add input process
    点の格納処理を追加しました。

--- compliment.cpp
+++ compliment.cpp
@@ -3,8 +3,8 @@
 #include<fstream>
 #include<vector>
 
-#define X 100
-#define Y 100
+#define ROW 5
+#define COL 50
 
 using namespace std;
 
@@ -17,13 +17,28 @@ struct Point{
 
 int main()
 {
-    string filename = "53394640_dsm_1m.dat";
+    string filename = "250_records.txt";
     ifstream file_in{filename}; //-std=c++17でコンパイル可能（file_in)
     if(!file_in){
         cerr << "ファイルを開けません："<< filename << "\n";
         exit(1);
     }
-    vector<vector<Point>> geometry(X, vector<Point>(Y));
-    string line;
+    vector<vector<Point>> geometry(ROW, vector<Point>(COL));
 
+    // 点の格納
+    for (int i = 0; i < ROW; ++i)
+    {
+        for (int j = 0; j < COL; ++j)
+        {
+            file_in >> geometry[i][j].x >> geometry[i][j].y >> geometry[i][j].z;
+        }
+    }
+
+    for (int i = 0; i < ROW; ++i)
+    {
+        for (int j = 0; j < COL; ++j)
+        {
+            cout << geometry[i][j].x << ' ' << geometry[i][j].y << ' ' << geometry[i][j].z << endl;
+        }
+    }
 }
