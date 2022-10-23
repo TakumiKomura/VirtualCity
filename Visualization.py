01fbdac4295c9c3f9af39767967ed3f93da6855e
r-tagu <rtagugtb.v001ms@outlook.jp>
2022-10-24 00:26:29 +0900

    create reading file
    これから変更するかもしれませんが今の所、迷ったあげくリストに落ち着きました。

--- Visualization.py
+++ Visualization.py
@@ -1,1 +1,14 @@
 #Delaunay triangulation and convert to VRML file
+import numpy as np
+from scipy.spatial import Delaunay
+
+#ファイルの読み込み(データはリストで管理)
+file_data=input("Enter the File name\n")
+point=[]
+with open(file_data) as f:
+    lines=f.readlines()
+    for line in lines:
+        pnt=line.split()
+        point.append([float(pnt[0]),float(pnt[1]),float(pnt[2]),int(pnt[3])])
+    
+#print(point)
