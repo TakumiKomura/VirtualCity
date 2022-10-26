bd4570775cccff45fab64f5216f763d8e56a895a
r-tagu <rtagugtb.v001ms@outlook.jp>
2022-10-26 19:01:10 +0900

    create points of delaunay
    三角形分割までを行えます。
    最後のprint分は確認なので気にしないでください。

--- Visualization.py
+++ Visualization.py
@@ -12,3 +12,12 @@ with open(file_data) as f:
         point.append([float(pnt[0]),float(pnt[1]),float(pnt[2]),int(pnt[3])])
     
 #print(point)
+
+point_delaunay=[]
+for i in range(len(point)):
+    point_delaunay.append([point[i][0],point[i][1]])
+
+point_delaunay_2=np.array(point_delaunay)
+tri=Delaunay(point_delaunay_2)
+#print(point_delaunay_2)
+print(tri.simplices)
